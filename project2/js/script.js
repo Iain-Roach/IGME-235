c3cab283f63935f28809766cd2930f609802aa8b
Iain-Roach <Roach.iain@gmail.com>
2021-11-08 14:17:22 -0500

    Start diving everyting

--- project2/js/script.js
+++ project2/js/script.js
@@ -6,10 +6,9 @@ function buttonClicked(){
     console.log("ButtonClicked");
 
     const BASE_URL = "https://www.dnd5eapi.co/api/spells/";
-
     let url = BASE_URL;
 
-    let term = document.querySelector("#spellterm").value;
+    let term = document.querySelector("#spellTerm").value;
     displayTerm = term;
 
     term = term.trim();
@@ -18,7 +17,8 @@ function buttonClicked(){
 
     term = term.replaceAll(' ', '-');
 
-    if(term.length < 1) return;
+    // if(term.length < 1) 
+    // return;
 
     url += term;
 
@@ -51,13 +51,63 @@ function dataLoaded(e) {
         return;
     }
 
+    //Loop through this for each item and create a new spellcard div
     let description = obj.desc;
     console.log("Desc: " + obj.desc);
     let p = document.querySelector("#description");
+    let name = document.querySelector("#spellName");
+    let level = document.querySelector("#spellLevel");
+    let castTime = document.querySelector("#castingTime");
+    let range = document.querySelector("#range");
+    let duration = document.querySelector("#duration");
+    let components = document.querySelector("#components");
+
+    name.innerHTML = obj.name;
+
+    let suffixNum = "";
+    suffixNum = suffixify(obj.level);
+    level.innerHTML = obj.level + suffixNum + " level " + obj.school.name;
+
+    castTime.innerHTML = obj.casting_time;
+
+    range.innerHTML = "Range: " + obj.range;
+
+    duration.innerHTML = "Duration: " + obj.duration;
+
+    let test = "";
+    obj.components.forEach(component => test += component + " ");
+    components.innerHTML = "Components: " + test;
+
     p.innerHTML = obj.desc;
 }
 
 function dataError(e) {
     console.log("An error occured");
 }
-//go from MaGic MissiLe to magic-missile
+
+function suffixify(lvl)
+{
+    if(lvl == "1")
+    {
+        return suffixNum = "st";
+    }
+    else if(lvl == "2")
+    {
+        return suffixNum = "nd";
+    }
+    else if(lvl == "3")
+    {
+        return suffixNum = "rd";
+    }
+    else 
+    {
+        return suffixNum = "th";
+    }
+}
+
+function createSpellCard()
+{
+    //Creates a div wit all according inside elements
+    let div = document.createElement('div');
+    div.class = 'spellCard';
+}
